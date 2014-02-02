2aad5dad3df6b44dcfbb3bdf2f81a055e3eaed9c
Sander Decoster <mail@sandec.net>
2014-02-02 16:24:12 +0100

    Basiswebsite toegevoegd met bootstrap, jQuery en Spacelab theme

---
+++ CursusPhp/Basis website/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Cursus PHP Basiswebsite</title>
+
+        <!-- Stylesheets: bootstrap, bootstrap theme & eigen stijlblad -->
+        <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.spacelab.min.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/eigenstijl.css" />
+
+    </head>
+    <body>
+
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Afsprakenplanner</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="spacer"></div>
+        
+        <div class="container">
+            <h1>Afsprakenplanner</h1>
+            <p class="lead">Welkom op uw afsprakenplanner website! Hier kan u al uw afspraken plannen.</p>
+            
+        </div>
+
+        <!-- Javascript files staan op het einde voor snellere laadtijden -->
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+    </body>
+</html>
