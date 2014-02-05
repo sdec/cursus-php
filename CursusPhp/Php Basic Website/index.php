fb0db15786c88ddb6982b0565c29df37c1f3b480
DavidAndrianneMobApp <David.andrianne@student.khleuven.be>
2014-02-05 13:25:07 +0100

    Php slechte basis website zonder enige framework patterns

---
+++ CursusPhp/Php Basic Website/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Basic Phpsite - Home</title>
+        <?php include_once('navbar.php'); ?>
+            <div class="jumbotron">
+                <h1>Afsprakenplanner - BASIC</h1>
+                <p class="lead">
+                    Deze website is de Php BASIC versie van de afsprakenplanner die jullie doorheen de cursus zullen herbouwen/uitbreiden tot een OO-design.
+                </p>
+                <p>
+                    <a href="appointments/viewappointments.php" class="btn btn-primary"><span class="glyphicon glyphicon-list"></span> Mijn afspraken</a> 
+                    <a href="appointments/createappointment.php" class="btn btn-primary"><span class="glyphicon glyphicon-plus-sign"></span> Maak een afspraak</a>
+                </p>
+            </div>
+        <?php include_once('footer.php'); ?>
