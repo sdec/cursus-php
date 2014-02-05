7cd8850b747b36ab185e70d4b1571b5b0fb6af80
Sander Decoster <mail@sandec.net>
2014-02-05 13:09:50 +0100

    Database redesign

--- CursusPhp/Basis website/database.sql
+++ CursusPhp/Basis website/database.sql
@@ -1,51 +1,85 @@
-DROP TABLE IF EXISTS appointments;
-DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS appointmentcourses;
+DROP TABLE IF EXISTS appointmentlecturers;
 DROP TABLE IF EXISTS courses;
-DROP TABLE IF EXISTS lecturers;
+DROP TABLE IF EXISTS appointments;
+DROP VIEW IF EXISTS administrators;
+DROP VIEW IF EXISTS studentadvisors;
+DROP VIEW IF EXISTS lecturers;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
+
     userid INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(32) NOT NULL,
     firstname VARCHAR(32) NOT NULL,
     lastname VARCHAR(32) NOT NULL,
     password VARCHAR(129) NOT NULL,
     email VARCHAR(128) NOT NULL,
+    accesslevel TINYINT UNSIGNED NOT NULL DEFAULT 0,
+
     PRIMARY KEY(userid)
 
 ) ENGINE=InnoDB;
 
-CREATE TABLE lecturers (
-    lecturerid INTEGER UNSIGNED NOT NULL,
-    userid INTEGER UNSIGNED NOT NULL,
-    PRIMARY KEY(lecturerid),
-    FOREIGN KEY(userid) REFERENCES users(userid) ON DELETE CASCADE ON UPDATE CASCADE
-);
+CREATE VIEW lecturers AS
+    SELECT *
+    FROM users
+    WHERE accesslevel >= 1;
 
-CREATE TABLE courses (
-    courseid INTEGER UNSIGNED NOT NULL,
-    name VARCHAR(32) NOT NULL,
-    PRIMARY KEY(courseid)
-);
+CREATE VIEW studentadvisors AS
+    SELECT *
+    FROM users
+    WHERE accesslevel >= 2;
 
-CREATE TABLE classes (
-    classid INTEGER UNSIGNED NOT NULL,
-    number VARCHAR(4) NOT NULL,
-    PRIMARY KEY(classid)
-);
+CREATE VIEW administrators AS 
+    SELECT *
+    FROM users
+    WHERE accesslevel >= 3;
 
 CREATE TABLE appointments (
-    appointmentid INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    userid INTEGER UNSIGNED NOT NULL,
-    lecturerid INTEGER UNSIGNED NOT NULL,
-    courseid INTEGER UNSIGNED NOT NULL,
-    classid INTEGER UNSIGNED NOT NULL,
-    description VARCHAR(256) NOT NULL,
-    due_date DATETIME NOT NULL,
 
-    PRIMARY KEY(appointmentid),
-    FOREIGN KEY(userid) REFERENCES users(userid) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(lecturerid) REFERENCES lecturers(lecturerid) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(courseid) REFERENCES courses(courseid) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(classid) REFERENCES classes(classid) ON DELETE CASCADE ON UPDATE CASCADE
-);
+    appointmentid INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    description TEXT NOT NULL,
+    start_timestamp DATETIME NOT NULL,
+    end_timestamp DATETIME NOT NULL,
+    location VARCHAR(32) NOT NULL,
+
+    PRIMARY KEY(appointmentid)
+
+) ENGINE=InnoDB;
+
+CREATE TABLE courses (
+
+    courseid INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    coursename VARCHAR(64) NOT NULL,
+
+    PRIMARY KEY(courseid)
+
+) ENGINE=InnoDB;
+
+CREATE TABLE appointmentlecturers (
+    
+    appointmentlecturerid INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    appointmentid INTEGER UNSIGNED NOT NULL,
+    lecturerid INTEGER UNSIGNED NOT NULL,
+    start_timestamp DATETIME NOT NULL,
+    end_timestamp DATETIME NOT NULL,
+    interval_timestamp DATETIME NOT NULL,
+
+    PRIMARY KEY(appointmentlecturerid),
+    FOREIGN KEY(appointmentid) REFERENCES appointments(appointmentid),
+    FOREIGN KEY(lecturerid) REFERENCES users(userid)
+
+) ENGINE=InnoDB;
+
+CREATE TABLE appointmentcourses (
+    
+    appointmentcourseid INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    appointmentid INTEGER UNSIGNED NOT NULL,
+    courseid INTEGER UNSIGNED NOT NULL,
+
+    PRIMARY KEY(appointmentcourseid),
+    FOREIGN KEY(appointmentid) REFERENCES appointments(appointmentid),
+    FOREIGN KEY(courseid) REFERENCES courses(courseid)
+
+) ENGINE=InnoDB;
