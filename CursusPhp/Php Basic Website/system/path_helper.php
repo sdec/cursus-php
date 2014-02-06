defaffd4653ddad53313038312edb67af5a4fd7e
DavidAndrianneMobApp <David.andrianne@student.khleuven.be>
2014-02-06 14:31:20 +0100

    $projectdir remake naar path_helper.php v2.0

--- CursusPhp/Php Basic Website/system/path_helper.php
+++ CursusPhp/Php Basic Website/system/path_helper.php
@@ -1,27 +1,15 @@
 <?php
+
 define('APP_PATH', '/cursus-php/CursusPhp/Php Basic Website/');
 
 function base_url() {
     return $_SERVER['HTTP_HOST'] . APP_PATH;
-
-function local_url(){
-    $cutDelim = "CursusPhp/Php Basic Website/";
-    return ($variable = substr($_SERVER['SCRIPT_FILENAME'], 0, strpos($_SERVER['SCRIPT_FILENAME'], $cutDelim)) . $cutDelim);
 }
 
-function host_url(){
-    //$urlPieces = explode('/', $_SERVER['PHP_SELF']);
-    //$page = $urlPieces();
-    //return str_replace("path_helper.php", "", $_SERVER['PHP_SELF']);
-    //return $_SERVER['HTTP_HOST'] . '/cursus-php/CursusPhp/Php Basic Website/';
+function assets_url() {
+    return base_url() . 'assets/';
 }
 
-function assets_url(){
-    return host_url() . 'assets/';
+function partials_url() {
+    return base_url() . 'partials/';
 }
-
-function partials_url(){
-    return local_url() . 'partials/';
-}
-
-?>
