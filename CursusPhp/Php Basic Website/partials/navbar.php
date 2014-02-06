208b675088fafa0e6430056a356427726bbdf42b
DavidAndrianneMobApp <David.andrianne@student.khleuven.be>
2014-02-06 14:49:18 +0100

    path_helper.php codeligniter update

--- CursusPhp/Php Basic Website/partials/navbar.php
+++ CursusPhp/Php Basic Website/partials/navbar.php
@@ -4,19 +4,19 @@
         <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div class="container">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="<?= host_url(); ?>index.php">Php basic</a>
+                    <a class="navbar-brand" href="<?= base_url(); ?>index.php">Php basic</a>
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav">
-                        <li><a href="<?= host_url(); ?>index.php">Index.php</a></li>
+                        <li><a href="<?= base_url(); ?>index.php">Index.php</a></li>
                         <li class="dropdown">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Database interactions <span class="caret"></span></a>
                             <ul class="dropdown-menu" aria-labelledby="themes">
-                                <li><a href="<?= host_url(); ?>appointments/viewappointments.php">viewDB.php</a></li>
-                                <li><a href="<?= host_url(); ?>appointments/createappointment.php">add.php</a></li>
+                                <li><a href="<?= base_url(); ?>appointments/viewappointments.php">viewDB.php</a></li>
+                                <li><a href="<?= base_url(); ?>appointments/createappointment.php">add.php</a></li>
                             </ul>
                         </li>
-                        <li><a href="<?= host_url(); ?>availability.php">queries.php</a></li>
+                        <li><a href="<?= base_url(); ?>availability.php">queries.php</a></li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li>
@@ -33,10 +33,10 @@
                                 <li><strong><?php if(!isset($_SESSION['user']['username'])){ echo "Niet ingelogd"; } else { echo "Ingelogd";}?></strong></li>
                                 <li class="nav-divider"></li>
                                 <?php if(isset($_SESSION['user']['username'])): ?>
-                                    <li><a href="<?= host_url(); ?>login.php?logout=true">logout(login.php)</a></li>
+                                <li><a href="<?= base_url(); ?>login.php?logout=true">logout(login.php)</a></li>
                                 <?php else: ?>
-                                    <li><a href="<?= host_url(); ?>login.php">login.php</a></li>
-                                    <li><a href="<?= host_url(); ?>register.php">Register.php</a></li>
+                                    <li><a href="<?= base_url(); ?>login.php">login.php</a></li>
+                                    <li><a href="<?= base_url(); ?>register.php">Register.php</a></li>
                                 <?php endif; ?>
                             </ul>
                         </li>
--- CursusPhp/Php Basic Website/system/path_helper.php
+++ CursusPhp/Php Basic Website/system/path_helper.php
@@ -1,9 +1,22 @@
 <?php
 
-define('APP_PATH', '/cursus-php/CursusPhp/Php Basic Website/');
+//define('APP_PATH', '/cursus-php/CursusPhp/Php Basic Website/');
 
 function base_url() {
-    return $_SERVER['HTTP_HOST'] . APP_PATH;
+    //return 'http://' . $_SERVER['HTTP_HOST'] . APP_PATH;
+    if (isset($_SERVER['HTTP_HOST'])) {
+        $base_url = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+        $base_url .= '://' . $_SERVER['HTTP_HOST'];
+        $base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+    } else {
+        $base_url = 'http://localhost/';
+    }
+    return $base_url;
+}
+
+function local_url(){
+    $cutDelim = "CursusPhp/Php Basic Website/";
+    return ($variable = substr($_SERVER['SCRIPT_FILENAME'], 0, strpos($_SERVER['SCRIPT_FILENAME'], $cutDelim)) . $cutDelim);
 }
 
 function assets_url() {
@@ -11,5 +24,5 @@ function assets_url() {
 }
 
 function partials_url() {
-    return base_url() . 'partials/';
+    return local_url() . 'partials/';
 }
