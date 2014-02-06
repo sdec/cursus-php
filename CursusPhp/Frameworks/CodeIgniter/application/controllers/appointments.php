ad05c3ec178849f04309bc86167256869af65426
Sander Decoster <mail@sandec.net>
2014-02-06 13:35:23 +0100

    Meer configuratie, appointments controller als default route & opmaak van de view pagina

--- CursusPhp/Frameworks/CodeIgniter/application/controllers/appointments.php
+++ CursusPhp/Frameworks/CodeIgniter/application/controllers/appointments.php
@@ -1,14 +1,14 @@
 <?php
 
-class Home extends CI_Controller {
+class Appointments extends CI_Controller {
     
     public function index() {
         
         if(!$this->session->userdata('user'))
             redirect('profile/login');
         
-        $this->template->write('title', 'Home');
-        $this->template->write_view('content', 'home');
+        $this->template->write('title', 'Afsprakenplanner');
+        $this->template->write_view('content', 'appointments/view');
         $this->template->render();
     }
     
