51bff7d40cf933b4b6fa1ef6a0dd225d75996c25
Sander Decoster <mail@sandec.net>
2014-02-06 15:26:52 +0100

    Lijst van afspraken uitbreiding met search box

--- CursusPhp/Frameworks/CodeIgniter/application/views/appointments/view.php
+++ CursusPhp/Frameworks/CodeIgniter/application/views/appointments/view.php
@@ -11,8 +11,8 @@
         </tr>
     </thead>
     <tbody>
-        <?php if(count($appointments) > 0) { ?>
-            <?php foreach($appointments as $appointment) { ?>
+        <?php if (count($appointments) > 0) { ?>
+            <?php foreach ($appointments as $appointment) { ?>
                 <tr>
                     <td><?= $appointment->date ?></td>
                     <td><?= $appointment->start ?></td>
@@ -27,3 +27,23 @@
         <?php } ?>
     </tbody>
 </table>
+
+<div class="row">
+    <div class="col-lg-3 col-md-3 col-sm-3">
+        <?php if ($this->session->userdata('user')->accesslevel >= LECTURER) { ?>
+            <p><a href="<?= base_url() ?>appointments/create" class="btn btn-primary">Nieuwe afspraak</a></p>
+        <?php } ?>
+    </div>
+    <div class="col-lg-9 col-md-9 col-sm-9">
+        <form class="form-horizontal" method="get" action="<?= base_url() ?>appointments/index" role="form">
+            <div class="form-group">
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Zoek afspraken op omschrijving, organisator, locatie, vakken, ..." />
+                </div>
+                <div class="col-sm-2">
+                    <input type="submit" class="form-control btn btn-primary" placeholder="Email" value="Zoek" />
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
--- CursusPhp/Frameworks/CodeIgniter/application/views/profile/login.php
+++ CursusPhp/Frameworks/CodeIgniter/application/views/profile/login.php
@@ -27,11 +27,6 @@
                     <button type="submit" class="btn btn-primary">Log in</button> 
                 </div>
             </div>
-            <div class="form-group">
-                <div class="col-lg-10 col-lg-offset-2">
-                
-                </div>
-            </div>
         </fieldset>
     </form>
 </div>
