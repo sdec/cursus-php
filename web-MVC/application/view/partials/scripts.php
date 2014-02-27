8bc0cb525663d4347c6849e8d389b5a910fa6633
sdec <mail@sandec.net>
2014-02-27 12:34:35 +0100

    MVC dynwebs framework refactoring en template (deels)

--- web-MVC/application/view/partials/scripts.php
+++
@@ -1,3 +0,0 @@
-<!-- Javascript files staan op het einde voor snellere laadtijden -->
-<script src="<?= assets_url() ?>js/jquery-1.11.0.min.js"></script>
-<script src="<?= assets_url() ?>bootstrap/js/bootstrap.min.js"></script>
